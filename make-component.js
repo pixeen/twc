8fbe6846ce791646e7e87a0b7f1e0270aa5b73bf
Jeroen Gerits <jeroen@gerits.email>
2024-10-05 22:45:06 +0200

    Refactor makeComponent function
    Extracted repetitive logic into modular helper functions

--- make-component.js
+++ make-component.js
@@ -1,32 +1,36 @@
-export const makeComponent =
-    (
-        name,
-        base,
-        variants = (theme) => ({ _: {} }),
-        defaultsVariants = (theme) => ({}),
-    ) =>
-    (theme) => {
-        return Object.entries(variants(theme)).reduce(
-            (acc, [variantGroup, variantGroupItems]) => {
-                Object.entries(variantGroupItems).forEach(
-                    ([variantKey, variantStyle]) => {
-                        acc[
-                            `[style-as="${name}"][with-${variantGroup}="${variantKey}"]`
-                        ] = variantStyle;
-                        acc[`.${name}--${variantGroup}-${variantKey}`] =
-                            variantStyle;
-                    },
-                );
-                acc[`[style-as="${name}"]`] = {
-                    ...base(theme),
-                    ...defaultsVariants(theme),
-                };
-                acc[`.${name}`] = {
-                    ...base(theme),
-                    ...defaultsVariants(theme),
-                };
-                return acc;
-            },
-            {},
-        );
+const createVariantSelector = (name, variantGroup, variantKey) =>
+    `[style-as="${name}"][with-${variantGroup}="${variantKey}"]`;
+
+const createVariantClass = (name, variantGroup, variantKey) =>
+    `.${name}--${variantGroup}-${variantKey}`;
+
+const applyBaseAndDefaultStyles = (acc, baseStyles, defaultStyles, componentName) => {
+    const baseSelector = `[style-as="${componentName}"]`;
+    const baseClass = `.${componentName}`;
+
+    acc[baseSelector] = {...baseStyles, ...defaultStyles};
+    acc[baseClass] = {...baseStyles, ...defaultStyles};
+
+    return acc;
+};
+
+export const makeComponent = (
+    componentName,
+    baseStyle,
+    variantStyles = (theme) => ({_: {}}),
+    defaultVariantStyles = (theme) => ({})
+) => (theme) => {
+    const baseStyles = baseStyle(theme);
+    const defaultStyles = defaultVariantStyles(theme);
+
+    const accumulateVariantStyles = (acc, [variantGroup, variantGroupItems]) => {
+        Object.entries(variantGroupItems).forEach(([variantKey, variantStyle]) => {
+            acc[createVariantSelector(componentName, variantGroup, variantKey)] = variantStyle;
+            acc[createVariantClass(componentName, variantGroup, variantKey)] = variantStyle;
+        });
+
+        return applyBaseAndDefaultStyles(acc, baseStyles, defaultStyles, componentName);
     };
+
+    return Object.entries(variantStyles(theme)).reduce(accumulateVariantStyles, {});
+};
