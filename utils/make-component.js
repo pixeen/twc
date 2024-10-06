f018495b363b21cdc73c91709bddce9c5eda340b
Jeroen Gerits <jeroen@gerits.email>
2024-10-06 04:00:48 +0200

    Add memoize and replace utilities, refactor makeComponent
    Add new `memoize` and `replace` utilities to optimize function execution and string replacement. Refactor `makeComponent` to use these utilities, enhancing performance and code readability.

--- utils/make-component.js
+++ utils/make-component.js
@@ -1,71 +1,36 @@
-const DEFAULT_COMPONENT_ATTRIBUTE = "style-as";
-const DEFAULT_VARIANT_ATTRIBUTE = "with-";
+import { memoize } from "./memoize.js";
+import { replace } from "./replace.js";
 
-const createSelector = {
-  componentClass: (component) => `.${component}`,
-  componentAttr: (component, attr) => `[${attr}="${component}"]`,
-  variantClass: (component, group, key) => `.${component}--${group}-${key}`,
-  variantAttr: (component, group, key, compAttr, varAttr) =>
-    `[${compAttr}="${component}"][${varAttr}${group}="${key}"]`,
-};
-
-const accumulateStyles = (
-  acc,
-  [group, items],
-  component,
-  theme,
-  baseFn,
-  defaultFn,
-  { attributes, classes },
-) => {
-  const base = baseFn(theme),
-    defaults = defaultFn(theme);
-  Object.entries(items).forEach(([key, style]) => {
-    acc[
-      createSelector.componentAttr(
-        component,
-        attributes.component ?? DEFAULT_COMPONENT_ATTRIBUTE,
-      )
-    ] = { ...base, ...defaults };
-    acc[createSelector.componentClass(component)] = { ...base, ...defaults };
-    if (attributes.enabled) {
-      acc[
-        createSelector.variantAttr(
-          component,
-          group,
-          key,
-          attributes.component ?? DEFAULT_COMPONENT_ATTRIBUTE,
-          attributes.variant ?? DEFAULT_VARIANT_ATTRIBUTE,
-        )
-      ] = style;
-    }
-
-    if (classes.enabled) {
-      acc[createSelector.variantClass(component, group, key)] = style;
-    }
-  });
-
-  return acc;
-};
-
-export const makeComponent =
+export const makeComponent = memoize(
   (
     componentName,
     baseStylesFn = () => ({}),
     variantStylesFn = () => ({}),
     defaultStylesFn = () => ({}),
   ) =>
-  (theme, options) =>
-    Object.entries(variantStylesFn(theme)).reduce(
-      (acc, variant) =>
-        accumulateStyles(
-          acc,
-          variant,
-          componentName,
-          theme,
-          baseStylesFn,
-          defaultStylesFn,
-          options,
-        ),
-      {},
-    );
+    (theme, options = {}) => {
+      return Object.entries(variantStylesFn(theme)).reduce(
+        (acc, [group, items = {}]) => {
+          return Object.entries(items).reduce((result, [key, variantStyle]) => {
+            const data = {
+              componentName: componentName,
+              variantName: group,
+              variantOption: key,
+            };
+            const componentSelector = replace(options.pattern.component, data);
+            const variantSelector = replace(options.pattern.variant, data);
+            const baseStyle = {
+              ...baseStylesFn(theme),
+              ...defaultStylesFn(theme),
+            };
+            return {
+              ...result,
+              [componentSelector]: baseStyle,
+              [variantSelector]: variantStyle,
+            };
+          }, acc);
+        },
+        {},
+      );
+    },
+);
