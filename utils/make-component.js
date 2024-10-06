e2e1a11c26964fd643f01515ce91625cbfce3d98
Jeroen Gerits <jeroen@gerits.email>
2024-10-06 04:02:52 +0200

    Remove docstrings from memoize and replace functions
    Removed the extensive documentation strings from `memoize.js` and `replace.js` files. This helps to streamline the source code and can improve readability while maintaining critical comments directly within the code as necessary.

--- utils/make-component.js
+++ utils/make-component.js
@@ -1,6 +1,15 @@
 import { memoize } from "./memoize.js";
 import { replace } from "./replace.js";
 
+/**
+ * A higher-order function that generates component styles based on theme and options.
+ *
+ * @param {string} componentName - The name of the component.
+ * @param {Function} baseStylesFn - Function that returns the base styles object.
+ * @param {Function} variantStylesFn - Function that returns the variant styles object.
+ * @param {Function} defaultStylesFn - Function that returns the default styles object.
+ * @returns {Function} - A function that takes a theme and options, then returns the computed styles.
+ */
 export const makeComponent = memoize(
   (
     componentName,
