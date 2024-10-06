00b64fbf536215658fee9f2076a8491d05d15fa3
Jeroen Gerits <jeroen@gerits.email>
2024-10-06 04:35:56 +0200

    Add JSDoc comments for memoize function
    Enhanced `component.js` by including detailed JSDoc comments for the memoize function. This update explains the purpose, input parameters, and return value, improving code readability and maintainability.

--- utils/component.js
+++ utils/component.js
@@ -1,6 +1,16 @@
 import { memoize } from "./memoize.js";
 import { replace } from "./replace.js";
 
+/**
+ * Memorizes a given function by caching its computed results.
+ *
+ * The `memoize` function takes another function `fn` as input and returns a new function
+ * that caches the results of `fn`. When the memoized function is called again with
+ * the same arguments, it returns the cached result instead of recomputing it.
+ *
+ * @param {Function} fn - The function to memoize.
+ * @returns {Function} The memoized version of `fn`.
+ */
 export default memoize(
   (
     componentName,
