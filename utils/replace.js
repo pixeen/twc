8983632e1a86de0334d853fb89c9618cd54623a3
Jeroen Gerits <jeroen@gerits.email>
2024-10-06 04:01:41 +0200

    Remove docstrings from memoize and replace functions
    Removed the extensive documentation strings from `memoize.js` and `replace.js` files. This helps to streamline the source code and can improve readability while maintaining critical comments directly within the code as necessary.

--- utils/replace.js
+++ utils/replace.js
@@ -1,13 +1,3 @@
-/**
- * Replaces placeholders within a given pattern string with corresponding values from a data object.
- *
- * This function searches for placeholders in the format `${key}` within the pattern string.
- * For each placeholder found, it replaces it with the value corresponding to the `key` in the provided `data` object.
- *
- * @param {string} pattern - The string containing placeholders to be replaced.
- * @param {Object} data - An object containing key-value pairs where the key matches the placeholder name in the pattern.
- * @returns {string} The resulting string with placeholders replaced by their corresponding values from the data object.
- */
 export const replace = (pattern, data) => {
   return pattern.replace(/\${(\w+)}/g, (_, match) => {
     return data[match];
