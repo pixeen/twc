3de4b6a308ba0cb5f6e997aeed875b8ae3d9e320
Jeroen Gerits <jeroen@gerits.email>
2024-10-08 20:57:35 +0200

    Add initial component template file
    Created a new JavaScript file for the component template in the specified directory. This initial version defines the structure for base, variants, defaultVariants, and compoundVariants properties.

---
+++ src/components/_template.js
@@ -0,0 +1,19 @@
+/**
+ * Component
+ *
+ * @version 0.0.1-dev
+ *
+ * Changelog:
+ * - 0.0.1-dev: Initial version
+ *
+ * Variants:
+ * - ...
+ *
+ */
+
+export default (theme) => ({
+  base: {},
+  variants: {},
+  defaultVariants: {},
+  compoundVariants: []
+})
